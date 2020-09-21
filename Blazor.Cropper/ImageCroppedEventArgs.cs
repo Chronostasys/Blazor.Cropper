a4b155160bd395413db8ff7431650d36f2450c40
博修 李 <1769712655@qq.com>
2020-09-21 10:05:26 +0800

    better OnCrop event

--- Blazor.Cropper/ImageCroppedEventArgs.cs
+++ Blazor.Cropper/ImageCroppedEventArgs.cs
@@ -1,1 +1,10 @@
-
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp;
+namespace Blazor.Cropper
+{
+    public class ImageCroppedEventArgs
+    {
+        public IImageFormat Format{get;set;}
+        public Image Image{get;set;}
+    }
+}
