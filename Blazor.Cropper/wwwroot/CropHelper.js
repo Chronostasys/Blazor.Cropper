fed79e44ff0b8f313ae9726bc1c8bbe49c7d8916
李博修 <liboxiu@dreamofalice.club>
2020-09-21 17:49:37 +0800

    add resize handler

--- Blazor.Cropper/wwwroot/CropHelper.js
+++ Blazor.Cropper/wwwroot/CropHelper.js
@@ -1,7 +1,29 @@
 // This file is to show how a library package may provide JavaScript interop features
 // wrapped in a .NET API
 
-function getWidth() {
-    console.log(document.getElementById("blazor_cropper").clientWidth)
-    return document.getElementById("blazor_cropper").clientWidth;
+function getWidthHeight() {
+    var a = [];
+    var e = document.getElementById("blazor_cropper");
+    a.push(e.clientWidth);
+    a.push(e.clientHeight);
+    return a;
 }
+async function cropAsync(id, sx, sy, swidth, sheight, x, y, width, height, format) {
+    var blob =  await new Promise(function (resolve) {
+        var canvas = document.createElement('canvas');
+        var img = document.getElementById(id);
+        canvas.width = width;
+        canvas.height = height;
+        canvas.getContext('2d').drawImage(img, sx, sy, swidth, sheight, x, y, width, height);
+        resolve(canvas.toDataURL(format));
+    })
+    
+    return blob;
+}
+window.addEventListener('resize', (ev) => {
+    try {
+        DotNet.invokeMethodAsync('Blazor.Cropper', 'SetWidth');
+    } catch (e) {
+        console.error(e);
+    }
+})
