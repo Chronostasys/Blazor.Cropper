8f8ef55c51022f7000e3f2093775ae6279a542ba
李博修 <liboxiu@dreamofalice.club>
2020-09-22 17:03:51 +0800

    add more properties to args

--- Blazor.Cropper/ImageCroppedEventArgs.cs
+++ Blazor.Cropper/ImageCroppedEventArgs.cs
@@ -6,5 +6,9 @@ namespace Blazor.Cropper
     {
         public IImageFormat Format{get;set;}
         public Image Image{get;set;}
+        public double X{get;set;}
+        public double Y{get;set;}
+        public double Width{get;set;}
+        public double Height{get;set;}
     }
 }
