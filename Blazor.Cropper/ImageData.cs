50efa3f8102cb636fb0af0f8e10b34bc2d030ed8
Tanvir Ahmad Arjel <tanvirarjel@outlook.com>
2021-08-01 12:01:32 +0600

    ImageCroppedResult.GetBase64Async() should return pure base64 string.

--- Blazor.Cropper/ImageData.cs
+++ Blazor.Cropper/ImageData.cs
@@ -1,6 +1,6 @@
 internal class ImageData
 {
-    public double Width{get;set;}
-    public double Height{get;set;}
+    public double Width { get; set; }
 
+    public double Height { get; set; }
 }
